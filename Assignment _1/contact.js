6485152a6ad627ad737f22261681ab34f726c299
Sreelakshmi A P <sreelakshmiperayil@gmail.com>
2022-05-13 12:29:19 +0530

    Assignment 1

--- Assignment _1/contact.js
+++ Assignment _1/contact.js
@@ -1,19 +1,31 @@
-function myfunction()
-{
-    alert("Ok")
-}
-function validate()
-{
-    var letter=/^[A-Za-z]+$/;
-    var name=document.getElementById(name).value;
-    if(name.match(letters))
+  function validation()
+  {
+    let Name = document.getElementById("Name").value;
+    let Phone = document.getElementById("Phone").value;
+    let Email = document.getElementById("Email").value;
+    let Msg = document.getElementById("Msg").value;
+    let regex_name = /^[a-zA-Z ]{2,30}$/;
+    let phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    let emailid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(!regex_name.test(Name))
     {
-        alert("valid name");
-        return true;
+      alert("inavlid name");
     }
-    else
+    else if(!phoneno.test(Phone))
     {
-        alert("inavlid name");
-        return false;
+      alert("Inavlid Phone number")
     }
-}
+    else if(!emailid.test(Email))
+    {
+      alert("Invalid Email");
+    }
+    else if(Msg.length == 0)
+    {
+      window.alert("Please type message");
+    }
+    else{
+      alert("Submitted Successfully")
+    }
+
+  } 
+    
